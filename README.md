cb870a4d5e5ebb7d9ba6e1a4b1219083c9c912a5
issohbog <qkrehgus2312@naver.com>
2025-06-23 17:58:01 +0900

    mvc, rest, swagger 완료

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AI3_SPRINGBOOT
 알클 AI3 스프링 부트
+
+
+## MyBatis
+
+```
+implementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3'
+    testImplementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3'
+```
---
+++ mvc/src/main/java/com/aloha/mvc/service/PostService.java
@@ -0,0 +1,18 @@
+package com.aloha.mvc.service;
+
+import java.util.List;
+
+import com.aloha.mvc.dto.Posts;
+
+public interface PostService {
+    // 목록
+    public List<Posts> list() throws Exception;
+    // 조회 
+    public Posts select(Integer no) throws Exception;
+    // 등록
+    public Boolean insert(Posts post) throws Exception;
+    // 수정
+    public Boolean update(Posts post) throws Exception;
+    // 삭제
+    public Boolean delete(Integer no) throws Exception;
+}
