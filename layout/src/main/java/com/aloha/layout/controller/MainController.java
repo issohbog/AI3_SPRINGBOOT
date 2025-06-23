413c79f85aebdb7ae5a00e04ee6df15e73acf7b8
issohbog <qkrehgus2312@naver.com>
2025-06-23 10:13:48 +0900

    타임리프, 레이아웃 실습

---
+++ layout/src/main/java/com/aloha/layout/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.aloha.layout.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    /**
+     * 메인 페이지 
+     */
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
