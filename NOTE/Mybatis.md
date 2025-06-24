60fa75b6c618f0e1a2c587a3ddacd7fcc6b38e63
issohbog <qkrehgus2312@naver.com>
2025-06-24 14:38:54 +0900

    페이지네이션(라이브러리X. 직접 수식 사용)

---
+++ NOTE/Mybatis.md
@@ -0,0 +1,6 @@
+###MyBatis 의존성
+
+```
+    implementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3'
+	testImplementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3'
+```
---
+++ page/src/main/java/com/aloha/page/service/PostService.java
@@ -0,0 +1,21 @@
+package com.aloha.page.service;
+
+import java.util.List;
+
+import com.aloha.page.domain.Pagination;
+import com.aloha.page.domain.Posts;
+
+public interface PostService {
+    // 목록
+    public List<Posts> list() throws Exception;
+    // 페이징 목록
+    public List<Posts> page(Pagination pagination) throws Exception;
+    // 조회 
+    public Posts select(Integer no) throws Exception;
+    // 등록
+    public Boolean insert(Posts post) throws Exception;
+    // 수정
+    public Boolean update(Posts post) throws Exception;
+    // 삭제
+    public Boolean delete(Integer no) throws Exception;
+}
